3928bb969c247ec8cb8d987a401f895322e7dae1
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-06-03 11:33:30 +0300

    feat: add main container

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,9 +1,11 @@
 import { Hero } from "./Hero/Hero";
+import { MainContainer } from "./MainContainer/MainContainer";
 
 export function Main() {
     return (
         <main>
             <Hero />
+            <MainContainer />
         </main>
     )
 }
---
+++ src/components/Main/MainContainer/MainContainer.tsx
@@ -0,0 +1,7 @@
+import styles from './MainContainer.module.css'
+
+export function MainContainer() {
+    return (
+        <div className={styles.container}></div>
+    )
+}
