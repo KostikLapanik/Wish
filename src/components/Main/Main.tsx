05e68b74dbe47ae9bd9370568317a6f45f82722a
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-06-03 14:09:54 +0300

    feat: add background to main

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,6 +1,8 @@
 import { Hero } from "./Hero/Hero";
 import { MainContainer } from "./MainContainer/MainContainer";
 
+import './Main.module.css'
+
 export function Main() {
     return (
         <main>
