fd261a789aba9066f4a3243f2ed092ff862a928b
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-06-03 14:21:00 +0300

    feat: add popular wishes section

--- src/components/Main/MainContainer/MainContainer.tsx
+++ src/components/Main/MainContainer/MainContainer.tsx
@@ -1,10 +1,12 @@
 import { HowItWorks } from './HowItWorks/HowItWorks'
 import styles from './MainContainer.module.css'
+import { PopularWishes } from './PopularWishes/PopularWishes'
 
 export function MainContainer() {
     return (
         <div className={styles.container}>
             <HowItWorks />
+            <PopularWishes />
         </div>
     )
 }
---
+++ src/components/Main/MainContainer/PopularWishes/PopularWishes.tsx
@@ -0,0 +1,10 @@
+import styles from './PopularWishes.module.scss'
+
+export function PopularWishes() {
+    return (
+        <section className={styles.popularWishes}>
+            <h2>Популярные желания</h2>
+
+        </section>
+    )
+}
