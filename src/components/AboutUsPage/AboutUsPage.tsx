61768767412fa41b23bc8d899e98f7eff9fef690
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-06-04 17:58:34 +0300

    feat: add why wish section

--- src/components/AboutUsPage/AboutUsPage.tsx
+++ src/components/AboutUsPage/AboutUsPage.tsx
@@ -1,11 +1,13 @@
 import { OurMission } from "./OurMission/OurMission";
 import { OurPrinciples } from "./OurPrinciples/OurPrinciples";
+import { WhyWish } from "./WhyWish/WhyWish";
 
 export function AboutUsPage() {
     return (
         <main>
             <OurMission />
             <OurPrinciples />
+            <WhyWish />
         </main>
     )
 }
---
+++ src/components/AboutUsPage/WhyWish/WhyWish.tsx
@@ -0,0 +1,39 @@
+import styles from './WhyWish.module.scss'
+
+export function WhyWish() {
+    return (
+        <section className={styles.whyWish}>
+            <h2>Почему стоит использовать Wish</h2>
+
+            <div className={styles.container}>
+                <div>
+                    <i className="fa-solid fa-circle-check"></i>
+                    <h4>Избегайте ненужных подарков</h4>
+                    <p>Получайте только то, что действительно хотите. Больше никаких бесполезных вещей,
+                        которые будут пылиться на полке.</p>
+                </div>
+
+                <div>
+                    <i className="fa-solid fa-gift"></i>
+                    <h4>Экономьте время на выборе</h4>
+                    <p>Не тратьте часы на раздумья - просто загляните в вишлист человека и выберите подарок,
+                        который точно ему понравится.</p>
+                </div>
+
+                <div>
+                    <i className="fa-solid fa-face-smile-wink"></i>
+                    <h4>Дарите с уверенностью</h4>
+                    <p>Будьте уверены, что ваш подарок придется по вкусу - ведь он был выбран
+                        самим получателем.</p>
+                </div>
+
+                <div>
+                    <i className="fa-solid fa-handshake"></i>
+                    <h4>Упростите процесс для всех</h4>
+                    <p>Больше не нужно гадать или намекать - просто добавьте желание в список
+                        и поделитесь им.</p>
+                </div>
+            </div>
+        </section>
+    )
+}
