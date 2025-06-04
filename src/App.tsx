5ca9984045e1fa9f77aa001b973d622ebbe728d9
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-06-04 18:30:58 +0300

    feat: change AboutUs page to Main page in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import { AboutUsPage } from "./components/AboutUsPage/AboutUsPage";
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
-// import { MainPage } from "./components/MainPage/MainPage";
+import { MainPage } from "./components/MainPage/MainPage";
 
 export function App() {
   return (
     <>
       <Header />
-      <AboutUsPage />
-      {/* <MainPage /> */}
+      <MainPage />
       <Footer />
     </>
   )
