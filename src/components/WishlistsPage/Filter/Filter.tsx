491fe2f6591dbf54f04a2e548feddcc6104492b5
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-06-05 13:26:43 +0300

    feat: add filter to wishlists page

--- src/components/WishlistsPage/Filter/Filter.tsx
+++ src/components/WishlistsPage/Filter/Filter.tsx
@@ -1,8 +1,81 @@
+import styles from './Filter.module.scss'
+
 export function Filter() {
     return (
-        <section>
+        <section className={styles.filter}>
             <form action="">
-                <select name="market" id="" title="Choose market"></select>
+                <div className={styles.marketSelector}>
+                    <label htmlFor="marketSelect" className={styles.title}>Select market</label>
+
+                    <div className={styles.options}>
+                        <div className={`${styles.option} ${styles.wb}`}>
+                            <i className="fa-solid fa-w"></i>
+                            <i className="fa-solid fa-b"></i>
+
+                            <input
+                                type="checkbox"
+                                id="wb"
+                                name="market"
+                                value="wb"
+                                title='wb'
+                            />
+
+                            <div className={styles.visual}></div>
+                            <div className={styles.effect}></div>
+                        </div>
+
+                        <div className={`${styles.option} ${styles.ozon}`}>
+                            <i className="fa-solid fa-o"></i>
+                            <i className="fa-solid fa-z"></i>
+                            <i className="fa-solid fa-o"></i>
+                            <i className="fa-solid fa-n"></i>
+
+                            <input
+                                type="checkbox"
+                                id="ozon"
+                                name="market"
+                                value="ozon"
+                                title='ozon'
+                            />
+
+                            <div className={styles.visual}></div>
+                            <div className={styles.effect}></div>
+                        </div>
+
+                        <div className={`${styles.option} ${styles.ym}`}>
+                            <i className="fa-solid fa-y"></i>
+                            <i className="fa-solid fa-m"></i>
+
+                            <input
+                                type="checkbox"
+                                id="ym"
+                                name="market"
+                                value="ym"
+                                title='ym'
+                            />
+
+                            <div className={styles.visual}></div>
+                            <div className={styles.effect}></div>
+                        </div>
+
+                        <div className={`${styles.option} ${styles.aliexpress}`}>
+                            <i className="fa-solid fa-a"></i>
+                            <i className="fa-solid fa-l"></i>
+                            <i className="fa-solid fa-i"></i>
+
+                            <input
+                                type="checkbox"
+                                id="aliexpress"
+                                name="market"
+                                value="aliexpress"
+                                title='aliexpress'
+                            />
+
+                            <div className={styles.visual}></div>
+                            <div className={styles.effect}></div>
+                        </div>
+                    </div>
+                </div>
 
                 <label htmlFor="price"></label>
                 <input type="number" id="price" title="Price" placeholder="Price" />
