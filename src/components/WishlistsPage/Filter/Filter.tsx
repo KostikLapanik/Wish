f3d16e73cefd929e3a3516b68cca011dfcf4c4e6
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-06-05 18:47:32 +0300

    feat: add wishes section add made logic of visibility/invisibility of filters

--- src/components/WishlistsPage/Filter/Filter.tsx
+++ src/components/WishlistsPage/Filter/Filter.tsx
@@ -1,41 +1,66 @@
-import { useState } from 'react';
+import { useState, type Dispatch, type SetStateAction } from 'react';
 import { markets } from '../../../data';
 import { Market } from './Market/Market';
 import { PriceFilter } from './PriceFilter/PriceFilter';
 
 import styles from './Filter.module.scss'
 
-export function Filter() {
+type Props = {
+    isFiltersOpen: boolean,
+    setIsFiltersOpen: Dispatch<SetStateAction<boolean>>
+}
+
+export function Filter({ isFiltersOpen, setIsFiltersOpen }: Props) {
     const [country, setCountry] = useState('');
 
+    const filtersSection = document.querySelector(`.${styles.filter}`);
+
+    if (isFiltersOpen && filtersSection) {
+        filtersSection.style.transform = 'translateX(0)';
+    } else if (!isFiltersOpen && filtersSection) {
+        filtersSection.style.transform = 'translateX(-200%)';
+    }
+
     return (
-        <section className={styles.filter}>
-            <form action="">
-                <div className={styles.marketSelector}>
-                    <label htmlFor="marketSelect" className={styles.title}>Select market</label>
+        <>
+            <button className={styles.filterBtn} type="button" onClick={() => setIsFiltersOpen(prev => !prev)}>
+                <i className="fa-solid fa-filter"></i>
+            </button>
 
-                    <div className={styles.options}>
-                        {markets.map((market: string) => {
-                            return <Market marketSigh={market} />
-                        })}
+            <section className={styles.filter}>
+                <form action="">
+                    <div className={styles.marketSelector}>
+                        <label htmlFor="marketSelect" className={styles.title}>Select market</label>
+
+                        <div className={styles.options}>
+                            {markets.map((market: string, i: number) => {
+                                return <Market marketSigh={market} key={i} />
+                            })}
+                        </div>
                     </div>
-                </div>
 
-                <PriceFilter />
+                    <PriceFilter />
 
-                <div className={styles.inputGroup}>
-                    <label htmlFor="country">Введите страну получателя:</label>
-                    <input type='text' name='country' title='country' id='country'
-                        onChange={e => setCountry(e.currentTarget.value)} value={country} />
-                </div>
+                    <div className={styles.inputGroup}>
+                        <label htmlFor="country">Введите страну получателя:</label>
+                        <input type='text' name='country' title='country' id='country'
+                            onChange={e => setCountry(e.currentTarget.value)} value={country} />
+                    </div>
 
-                <div className={styles.inputGroup}>
-                    <label htmlFor="ownerName">Введите имя получателя:</label>
-                    <input type="text" id="ownerName" title="ownerName" />
-                </div>
+                    <div className={styles.inputGroup}>
+                        <label htmlFor="ownerName">Введите имя получателя:</label>
+                        <input type="text" id="ownerName" title="ownerName" />
+                    </div>
 
-                <button type='submit'>Применить</button>
-            </form>
-        </section>
+                    <div className={styles.actions}>
+                        <button type='submit'>Применить</button>
+
+                        <button type='button' className={styles.closeBtn} onClick={() => setIsFiltersOpen(prev => !prev)}>
+                            <i className="fa-solid fa-xmark"></i>
+                        </button>
+                    </div>
+                </form>
+            </section>
+        </>
     )
 }
