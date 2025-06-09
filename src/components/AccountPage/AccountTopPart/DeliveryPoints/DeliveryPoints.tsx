4be288f5f9431ef1217a819668f8fa055dead330
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-06-09 15:21:03 +0300

    feat: add scroll logic

--- src/components/AccountPage/AccountTopPart/DeliveryPoints/DeliveryPoints.tsx
+++ src/components/AccountPage/AccountTopPart/DeliveryPoints/DeliveryPoints.tsx
@@ -1,82 +1,92 @@
+import { useRef } from 'react';
+
 import styles from './DeliveryPoints.module.css';
 
 export function DeliveryPoints() {
+    const scrollRef = useRef(null);
+    const scrollStep = 300;
+
+    const scrollLeft = () => {
+        scrollRef.current.scrollBy({
+            left: -scrollStep,
+            behavior: 'smooth'
+        });
+    };
+
+    const scrollRight = () => {
+        scrollRef.current.scrollBy({
+            left: scrollStep,
+            behavior: 'smooth'
+        });
+    };
+
     return (
-        <section className={styles.deliveryPoints}>
-            <div className={styles.deliveryPoint}>
-                <i className="fa-solid fa-map-pin"></i>
-                <p>Город, улица, дом</p>
+        <section className={styles.container}>
+            <i className={`fa-solid fa-arrow-left ${styles.arrow} ${styles.leftArrow}`} onClick={scrollLeft}></i>
+
+            <div className={styles.deliveryPoints} ref={scrollRef}>
+                <div className={styles.deliveryPoint}>
+                    <i className="fa-solid fa-map-pin"></i>
+                    <p>Город, улица, дом</p>
+                </div>
+
+                <div className={styles.deliveryPoint}>
+                    <i className="fa-solid fa-map-pin"></i>
+                    <p>Город, улица, дом</p>
+                </div>
+
+                <div className={styles.deliveryPoint}>
+                    <i className="fa-solid fa-map-pin"></i>
+                    <p>Город, улица, дом</p>
+                </div>
+
+                <div className={styles.deliveryPoint}>
+                    <i className="fa-solid fa-map-pin"></i>
+                    <p>Город, улица, дом</p>
+                </div>
+
+                <div className={styles.deliveryPoint}>
+                    <i className="fa-solid fa-map-pin"></i>
+                    <p>Город, улица, дом</p>
+                </div>
+
+                <div className={styles.deliveryPoint}>
+                    <i className="fa-solid fa-map-pin"></i>
+                    <p>Город, улица, дом</p>
+                </div>
+
+                <div className={styles.deliveryPoint}>
+                    <i className="fa-solid fa-map-pin"></i>
+                    <p>Город, улица, дом</p>
+                </div>
+
+                <div className={styles.deliveryPoint}>
+                    <i className="fa-solid fa-map-pin"></i>
+                    <p>Город, улица, дом</p>
+                </div>
+
+                <div className={styles.deliveryPoint}>
+                    <i className="fa-solid fa-map-pin"></i>
+                    <p>Город, улица, дом</p>
+                </div>
+
+                <div className={styles.deliveryPoint}>
+                    <i className="fa-solid fa-map-pin"></i>
+                    <p>Город, улица, дом</p>
+                </div>
+
+                <div className={styles.deliveryPoint}>
+                    <i className="fa-solid fa-map-pin"></i>
+                    <p>Город, улица, дом</p>
+                </div>
+
+                <div className={styles.deliveryPoint}>
+                    <i className="fa-solid fa-map-pin"></i>
+                    <p>Город, улица, дом</p>
+                </div>
             </div>
 
-            <div className={styles.deliveryPoint}>
-                <i className="fa-solid fa-map-pin"></i>
-                <p>Город, улица, дом</p>
-            </div>
-
-            <div className={styles.deliveryPoint}>
-                <i className="fa-solid fa-map-pin"></i>
-                <p>Город, улица, дом</p>
-            </div>
-
-            <div className={styles.deliveryPoint}>
-                <i className="fa-solid fa-map-pin"></i>
-                <p>Город, улица, дом</p>
-            </div>
-
-            <div className={styles.deliveryPoint}>
-                <i className="fa-solid fa-map-pin"></i>
-                <p>Город, улица, дом</p>
-            </div>
-
-            <div className={styles.deliveryPoint}>
-                <i className="fa-solid fa-map-pin"></i>
-                <p>Город, улица, дом</p>
-            </div>
-
-            <div className={styles.deliveryPoint}>
-                <i className="fa-solid fa-map-pin"></i>
-                <p>Город, улица, дом</p>
-            </div>
-
-            <div className={styles.deliveryPoint}>
-                <i className="fa-solid fa-map-pin"></i>
-                <p>Город, улица, дом</p>
-            </div>
-
-            <div className={styles.deliveryPoint}>
-                <i className="fa-solid fa-map-pin"></i>
-                <p>Город, улица, дом</p>
-            </div>
-
-            <div className={styles.deliveryPoint}>
-                <i className="fa-solid fa-map-pin"></i>
-                <p>Город, улица, дом</p>
-            </div>
-
-            <div className={styles.deliveryPoint}>
-                <i className="fa-solid fa-map-pin"></i>
-                <p>Город, улица, дом</p>
-            </div>
-
-            <div className={styles.deliveryPoint}>
-                <i className="fa-solid fa-map-pin"></i>
-                <p>Город, улица, дом</p>
-            </div>
-
-            <div className={styles.deliveryPoint}>
-                <i className="fa-solid fa-map-pin"></i>
-                <p>Город, улица, дом</p>
-            </div>
-
-            <div className={styles.deliveryPoint}>
-                <i className="fa-solid fa-map-pin"></i>
-                <p>Город, улица, дом</p>
-            </div>
-
-            <div className={styles.deliveryPoint}>
-                <i className="fa-solid fa-map-pin"></i>
-                <p>Город, улица, дом</p>
-            </div>
+            <i className={`fa-solid fa-arrow-right ${styles.arrow} ${styles.rightArrow}`} onClick={scrollRight}></i>
         </section>
     )
 }
